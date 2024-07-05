5f66cd4433aee9ad8870478036beba33105b7a8d
Rob-Yoo <jin12243@naver.com>
2024-07-05 14:30:22 +0900

    Chore: 폴더 구조 변경

---
+++ Shoppang!/Shoppang!/Sources/Presenter/Screens/CartListScreen/CartListViewController.swift
@@ -0,0 +1,29 @@
+//
+//  CartListViewController.swift
+//  Shoppang!
+//
+//  Created by Jinyoung Yoo on 7/5/24.
+//
+
+import UIKit
+
+class CartListViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ Shoppang!/Shoppang!/Sources/Presenter/Screens/CartListScreen/View/CartListRootView.swift
@@ -0,0 +1,20 @@
+//
+//  CartListRootView.swift
+//  Shoppang!
+//
+//  Created by Jinyoung Yoo on 7/5/24.
+//
+
+import UIKit
+
+class CartListRootView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
